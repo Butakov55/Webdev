48c4dd1aa3d759741aded490cfd40f54d24eb026
alexandr <butakovalexandr@mail.ru>
2021-03-05 13:32:05 +0600

    lesson2

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+// console.log("Hello world");
+// let myName = 'Alex';
+// let myAge = 33;
+// console.log('Мое имя: ' + myName);
+// console.log(`Мое имя: ${myName}`);
+// alert('Hello');
+document.querySelector('button').onclick = myClick;
+
+// document.querySelector('button').addEventListener('click', myClick);
+
+function myClick() {
+    let a = document.querySelector('.i-1').value;
+    console.log(a);
+    document.querySelector('.out').innerHTML = a;
+}
